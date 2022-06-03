2b7a5d529b186bec074e72d0fa695742dc0d390d
l22chi <71894045+l22chi@users.noreply.github.com>
2022-06-03 22:35:14 +0200

    add script to generate bg.txt

---
+++ classifiers-constructor/makeNeg.py
@@ -0,0 +1,16 @@
+import os
+
+def create_neg(path=os.path.dirname(os.path.realpath(__file__))):
+
+    data_dir = 'dataset/images/neg'
+
+    for img in os.listdir(str(path)+'/'+str(data_dir)):
+        if not img.startswith('.'):
+            if not img.endswith('.webp'):
+                if not img.endswith('.crdownload'):
+                    line = 'dataset/images/neg/' + str(img) + '\n'
+                    print(line)
+                    with open('dataset/images/bg.txt','a') as f:
+                        f.write(line)
+
+create_neg()
