d0c414028d43ce6832ea01590366e8bbd1c70b63
l22chi <71894045+l22chi@users.noreply.github.com>
2022-06-03 20:11:41 +0200

    OpenCV 3.4.0 stable version installation (working) FINALLY !!!

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
-[![](https://img.shields.io/badge/python-3.8.5-blue.svg?&logo=python&logoColor=yellow)](https://www.python.org/downloads/release/python-385/) [![](https://img.shields.io/badge/OpenCV-4.5.5-blue?&logo=opencv&logoColor=red)](https://opencv.org/) [![](https://img.shields.io/badge/Docker-20.10.14-blue?&logo=Docker&logoColor=blue)](https://opencv.org/)
+[![](https://img.shields.io/badge/python-3.8.5-blue.svg?&logo=python&logoColor=yellow)](https://www.python.org/downloads/release/python-385/) [![](https://img.shields.io/badge/OpenCV-3.4.0-blue?&logo=opencv&logoColor=red)](https://opencv.org/) [![](https://img.shields.io/badge/Docker-20.10.14-blue?&logo=Docker&logoColor=blue)](https://hub.docker.com/repository/docker/l22chi/opencv-ubuntu)
+
+
+The models built in this project use haarcascade features trained with OpenCV.
+
+Positive images were annotated with OpenCV (version 4.5.5, but works on 3.4.0 which is thee version required on this project) and the opencv_annotation tool (using a GUI).
+
+Concerning the training of the haarcascade and the creation of several positive samples, they were respectively carried out with the tools createsamples and trainhaarcascade of OpenCV. These two applications cannot be called with a classic installation of OpenCV (versions greater than 3.4.x) a docker image based on Ubuntu (latest) with a pre-download of the OpenCV version 3.4 archive is necessary (if you don't want to build the OpenCV library on your local environment).
+To use these tools it is necessary to build this image and use it via the CLI window of this one. For this you can refer to the documentation of the image on the Docker Hub (https://hub.docker.com/repository/docker/l22chi/opencv-ubuntu) or let yourself be guided by the automatic installation with initialize .sh
+
+# Automatic installation with initialize.sh
+
+
+
--- initialize.sh
+++ initialize.sh
@@ -24,10 +24,6 @@ if [[ "$OSTYPE" == "darwin"* ]]; then
     brew install virtualenv
     echo -e "${GREEN}[SUCCESS] Succefully installed${NC}"
 
-    echo -e "${YELLOW}[INFO] Installing opencv and all its dependencies${NC}"
-    brew install opencv
-    echo -e "${GREEN}[SUCCESS] Succefully installed${NC}"
-
     echo -e "${YELLOW}[INFO] Installing git and all its dependencies${NC}"
     brew install git
     echo -e "${GREEN}[SUCCESS] Succefully installed${NC}"
@@ -36,44 +32,46 @@ if [[ "$OSTYPE" == "darwin"* ]]; then
     brew install cmake
     echo -e "${GREEN}[SUCCESS] Succefully installed${NC}"
 
-    echo -e "${YELLOW}[INFO] Building OpenCV and compiling createsamples and trainhaarcascade${NC}"
-    git clone https://github.com/opencv/opencv.git
-    mkdir build_opencv
-    cd build_opencv
-    cmake -DCMAKE_BUILD_TYPE=Release -DBUILD_EXAMPLES=ON ../opencv
-    make -j7
-    echo -e "${GREEN}[SUCCESS] OpenCV core modules succefully compiled${NC}"
-    echo -e "${YELLOW}[WARNING] If your OpenCV version is above 3.4.x you won't be able to use createsamples and trainhaarcascade commands. To do so, build the docker image with OpenCV 3.4${NC}"
-    echo -e "${YELLOW}[WARNING] The annotation tool provided by OpenCV need a graphical interface and work on latest versions of OpenCV (above 3.4.x) but won't run on the docker image${NC}"
-    echo "Do you want to build the corresponding docker image ? [y/n]"
-    read YES_OR_NO
+    echo -e "${YELLOW}[INFO] Installing wget and all its dependencies${NC}"
+    brew install wget
+    echo -e "${GREEN}[SUCCESS] Succefully installed${NC}"
 
-    if [ "$YES_OR_NO" = "yes" ] || [ "$YES_OR_NO" = "y" ]; then
-        sudo docker build -t opencv-image docker
-        echo -e "${GREEN}[SUCCESS] Image succefully built. Please now use cmake to build OpenCV-3.4 archives. If get any troubles building it, please refers to the image doc on : ${NC}https://hub.docker.com/repository/docker/l22chi/opencv-ubuntu"
+    echo -e "${YELLOW}[INFO] Installing unzip and all its dependencies${NC}"
+    brew install unzip
+    echo -e "${GREEN}[SUCCESS] Succefully installed${NC}"
+
+    echo -e "${YELLOW}[INFO] Installing g++ and all its dependencies${NC}"
+    brew install g++
+    echo -e "${GREEN}[SUCCESS] Succefully installed${NC}"
+
+    echo -e "${YELLOW}[INFO] Building and installing OpenCV${NC}"
+    mkdir opencv && cd opencv
+    wget -O opencv.zip https://github.com/opencv/opencv/archive/3.4.zip
+    unzip opencv.zip
+    mkdir -p build && cd build
+    cmake  ../opencv-3.4
+    echo -e "${RED}[WARNING] Following make command is running with -j4 param (switch control to use 4 processes), please check your number of cores.${NC}"
+    make -j4
+    make install
+    echo -e "${GREEN}[SUCCESS] OpenCV core modules succefully compiled and installed${NC}"
 
 
 elif [[ "$OSTYPE" == "linux-gnu"* ]]; then
 
     sudo apt-get update
-    sudo apt install software-properties-common
-    sudo add-apt-repository ppa:deadsnakes/ppa
-    sudo apt update
 
     if ! python3 -c 'import sys; assert sys.version_info <= (3,7)' > /dev/null; then
-        sudo apt install python3.8
+        sudo apt-get install python3.8
     fi
 
     sudo apt-get install python3-pip
-    sudo apt-get install libopencv-dev
-    sudo apt-get install git
-
-    sudo apt install -y cmake g++ wget unzip
+    sudo mkdir opencv && cd opencv
+    sudo apt-get -y install git cmake g++ wget unzip
     wget -O opencv.zip https://github.com/opencv/opencv/archive/3.4.zip
     unzip opencv.zip
     mkdir -p build && cd build
     cmake  ../opencv-3.4
-    cmake --build .
+    make -j4make install
 
 fi
 
@@ -81,11 +79,16 @@ cd $SCRIPT_DIR
 
 echo -e "${RED}[WARNING] You need to have Docker installed to continue !${NC}"
 
-echo -e "${YELLOW}[INFO] Building Docker image FROM dockerfile : $CURRENT${NC}"
+echo -e "${YELLOW}[WARNING] If your OpenCV version is above 3.4.x you won't be able to use createsamples and trainhaarcascade commands. To do so, build the docker image with OpenCV 3.4${NC}"
+echo -e "${YELLOW}[WARNING] The annotation tool provided by OpenCV need a graphical interface and work on latest versions of OpenCV (above 3.4.x) but won't run on the docker image${NC}"
+echo "Do you want to build the corresponding docker image ? [y/n]"
+read YES_OR_NO
 
-sudo docker build -t opencv-image docker
-
-echo -e "${GREEN}[SUCCESS] Docker image has been built.${NC}"
+if [ "$YES_OR_NO" = "yes" ] || [ "$YES_OR_NO" = "y" ]; then
+    echo -e "${YELLOW}[INFO] Building Docker image FROM dockerfile : $CURRENT${NC}"
+    sudo docker build -t opencv-image docker
+    echo -e "${GREEN}[SUCCESS] Image succefully built. Please now use cmake to build OpenCV-3.4 archives. If get any troubles building it, please refers to the image doc on : ${NC}https://hub.docker.com/repository/docker/l22chi/opencv-ubuntu"
+fi
 
 echo -e "${RED}[WARNING] Executing this script will create a virtual environment venv in the current working directory !${NC}"
 
