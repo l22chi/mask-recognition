360ae26e7e085f2db54dae3e84daf9fc7ab61c59
l22chi <71894045+l22chi@users.noreply.github.com>
2022-06-09 16:05:49 +0200

    rebuilding CNN model : rebuilding the cnn model more properly with official doc and oop

---
+++ CNN-object-classifier/test.py
@@ -0,0 +1,33 @@
+import os
+import pandas as pd # type: ignore
+from torchvision.io import read_image
+import torch
+from torch.utils.data import Dataset
+from torchvision import datasets
+from torchvision.transforms import ToTensor
+import matplotlib.pyplot as plt
+
+
+class CustomImageDataset(Dataset):
+    def __init__(self, annotations_file, img_dir, transform=None, target_transform=None):
+        self.img_labels = pd.read_csv(annotations_file)
+        self.img_dir = img_dir
+        self.transform = transform
+        self.target_transform = target_transform
+
+    def __len__(self):
+        return len(self.img_labels)
+
+    def __getitem__(self, idx):
+        img_path = os.path.join(self.img_dir, self.img_labels.iloc[idx, 0])
+        image = read_image(img_path)
+        label = self.img_labels.iloc[idx, 1]
+        if self.transform:
+            image = self.transform(image)
+        if self.target_transform:
+            label = self.target_transform(label)
+        return image, label
+
+annotations = r'images/annotations.txt'
+image_location = r'images/global_data'
+data = CustomImageDataset(annotations, image_location)
