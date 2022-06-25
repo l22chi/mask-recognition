c179432bef95c76b6f0076c6bcf80ecdc99fc654
l22chi <71894045+l22chi@users.noreply.github.com>
2022-06-25 18:04:45 +0200

    add the object detection module built from pyimagesearch academy tutorial (used in the TF Keras model)

---
+++ object-detection-image/autorun.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+NC='\033[0m' # No Color
+YELLOW='\033[0;33m'
+
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+echo -e "${YELLOW}[INFO] Moving to the project folder : $SCRIPT_DIR ...${NC}"
+cd $SCRIPT_DIR
+
+echo -e "${YELLOW}[INFO] Running selective_search.py ...${NC}"
+echo "Please enter the name of the image you want to detect an object on (e.g. :image.jpg)"
+read IMAGE_NAME
+
+echo "Do you want to choose params ? (y/n)"
+read ANSWER
+if [[ "$ANSWER" == "y" ]]
+then
+    echo "Choose your minimum probability to consider a classification/detection (0 to 1, e.g. : 0.9)"
+    read MIN_PROB
+    echo -e "${YELLOW}[INFO] The minimum probability you chose is : $MIN_PROB${NC}"
+
+    echo "Choose the ROI size in pixeel, e.g. : (200, 150)"
+    read ROI_SIZ
+    echo -e "${YELLOW}[INFO] The ROI size you chose is : $ROI_SIZ${NC}"
+
+    echo "Whether or not to show extra visualizations for debugging ?"
+    read EXTRA_VIZU
+    echo -e "${YELLOW}[INFO] The exra visualizations you chose is : $EXTRA_VIZU${NC}"
+
+    echo -e "${YELLOW}[INFO] executing script ...${NC}"
+
+    python detect_with_classifier.py --image $IMAGE_NAME --size $ROI_SIZ --min-conf $MIN_PROB --visualize $EXTRA_VIZU
+
+elif [[ "$ANSWER" == "n" ]]; then
+    echo -e "${YELLOW}[INFO] executing script ...${NC}"
+    python detect_with_classifier.py --image $IMAGE_NAME
+fi
+
+echo -e "${YELLOW}[INFO] end of script.${NC}"
