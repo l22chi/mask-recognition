e3ace18c199b0b7d804d1a8c3c261d10adaa3086
l22chi <71894045+l22chi@users.noreply.github.com>
2022-06-25 18:46:48 +0200

    adding extra visualizations for debugging

--- object-detection-image/autorun.sh
+++ object-detection-image/autorun.sh
@@ -13,8 +13,8 @@ read IMAGE_NAME
 
 echo "Do you want to choose params ? (y/n)"
 read ANSWER
-if [[ "$ANSWER" == "y" ]]
-then
+if [[ "$ANSWER" == "y" ]]; then
+
     echo "Choose your minimum probability to consider a classification/detection (0 to 1, e.g. : 0.9)"
     read MIN_PROB
     echo -e "${YELLOW}[INFO] The minimum probability you chose is : $MIN_PROB${NC}"
@@ -23,15 +23,25 @@ then
     read ROI_SIZ
     echo -e "${YELLOW}[INFO] The ROI size you chose is : $ROI_SIZ${NC}"
 
-    echo "Whether or not to show extra visualizations for debugging ?"
+    echo "Whether or not to show extra visualizations for debugging (y/n) ?"
     read EXTRA_VIZU
-    echo -e "${YELLOW}[INFO] The exra visualizations you chose is : $EXTRA_VIZU${NC}"
 
-    echo -e "${YELLOW}[INFO] executing script ...${NC}"
+    if [[ "$EXTRA_VIZU" == "y" ]]; then
 
-    python detect_with_classifier.py --image $IMAGE_NAME --size $ROI_SIZ --min-conf $MIN_PROB --visualize $EXTRA_VIZU
+        echo "How much bounding box you want to see ? (to activate, must be at least >0)"
+        read BOUNDING_BOXES
+        echo -e "${YELLOW}[INFO] The exra visualizations you chose is : $BOUNDING_BOXES${NC}"
+        echo -e "${YELLOW}[INFO] executing script ...${NC}"
+        python detect_with_classifier.py --image $IMAGE_NAME --size $ROI_SIZ --min-conf $MIN_PROB --visualize $BOUNDING_BOXES
+
+    elif [[ "$EXTRA_VIZU" == "n" ]]; then
+
+        echo -e "${YELLOW}[INFO] executing script ...${NC}"
+        python detect_with_classifier.py --image $IMAGE_NAME --size $ROI_SIZ --min-conf $MIN_PROB
+    fi
 
 elif [[ "$ANSWER" == "n" ]]; then
+
     echo -e "${YELLOW}[INFO] executing script ...${NC}"
     python detect_with_classifier.py --image $IMAGE_NAME
 fi
